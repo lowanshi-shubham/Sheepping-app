d8cc406adad194b96457b8d8c7980c286551fa5e
shubham <shubhamlowanshi12345@gmail.com>
2025-07-20 15:06:06 +0530

    added whole code

---
+++ UI/src/components/ContactComponent/Contact.jsx
@@ -0,0 +1,54 @@
+import "./Contact.css";
+
+function Contact() {
+  return (
+    <>
+      {/* About Start */}
+      <div class="container-xxl py-5">
+        <div class="container">
+          <div class="row g-5 align-items-center">
+            <div class="col-lg-12">
+              <h6 class="section-title text-start text-primary text-uppercase">
+                Contact Page
+              </h6>
+              <h1 class="mb-4">
+                Welcome to{" "}
+                <span class="text-primary text-uppercase">Shipping War</span>
+              </h1>
+              <p class="mb-4" style={{ "text-align": "justify" }}>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vero
+                adipisci facilis delectus distinctio harum tenetur, voluptas
+                minima maxime ea dicta iure accusamus ratione alias at
+                doloribus, ullam modi quidem exercitationem nemo aliquid iste ex
+                suscipit! Ratione quibusdam nemo recusandae iste error? Nulla
+                dicta non eos totam dolorum distinctio earum eum fugiat commodi
+                officia. Minima repellat velit dolores dolore odio quis
+                necessitatibus quibusdam, voluptatem cum consectetur
+                reprehenderit odit reiciendis assumenda accusamus rerum
+                temporibus, iste beatae at fugiat quaerat inventore consequatur
+                sequi ipsa! Quisquam voluptas provident perferendis, soluta
+                eveniet sapiente alias doloribus similique itaque fuga incidunt
+                inventore expedita, eaque in, est voluptatum. Nulla, natus rem?
+                Officia animi consequatur sit minima delectus! At voluptatibus
+                reprehenderit quam dolorum necessitatibus, mollitia perspiciatis
+                dolorem repudiandae assumenda voluptate facere amet harum itaque
+                illo eaque vero fugit neque ut? Libero eum iste reiciendis
+                fugiat voluptatibus reprehenderit fuga nam non ducimus corrupti
+                repellat atque quos cum tempore, necessitatibus ullam. Repellat
+                eius minima reprehenderit libero excepturi inventore beatae
+                voluptatibus eaque quaerat dolorum dolores nemo voluptatum ex,
+                ad dicta id optio porro facere quidem. Sunt optio ad quisquam
+                delectus qui magni blanditiis? Eligendi mollitia, tempore
+                impedit nobis minus vitae sapiente deleniti a eius sed aperiam
+                optio autem explicabo consectetur saepe? Unde.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* About End */}
+    </>
+  );
+}
+
+export default Contact;
